dd9e8e5d693ad29bd2d1a4c3138910897650f302
xumc <hmilym@gmail.com>
2020-05-31 16:02:27 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/xumc/taildt
+
+go 1.12
+
+require (
+	github.com/aws/aws-sdk-go v1.31.7
+	github.com/olekukonko/tablewriter v0.0.4
+	github.com/xumc/go-dynamodb-stream-subscriber v0.0.0-20181123094412-d1f372e41da2
+)
---
+++ main.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodbstreams"
+	"github.com/olekukonko/tablewriter"
+	"github.com/xumc/go-dynamodb-stream-subscriber/stream"
+	"os"
+	"strings"
+	"sync"
+)
+
+type outputItem struct {
+	tableName string
+	action string
+	changes [][3]string
+}
+
+func main() {
+	typ := flag.String("type", "ddb", "ddb or mysql")
+	table := flag.String("table", "", "table names")
+
+	flag.Parse()
+
+	outputer := make(chan outputItem)
+	switch *typ {
+	case "ddb":
+		tailDDB([]string{*table}, outputer)
+	}
+
+
+	outputTo := os.Stdout
+	for o := range outputer {
+		fmt.Fprintln(outputTo)
+		fmt.Fprintf(outputTo, "%s	%s\n", o.action, o.tableName)
+
+		tw := tablewriter.NewWriter(outputTo)
+		tw.SetHeader([]string{"Name", "Old Value", "New Value"})
+		for _, c := range o.changes {
+			tw.Append(c[:3])
+		}
+		tw.SetRowLine(true)
+		tw.SetColMinWidth(1, 35)
+		tw.SetColMinWidth(2, 35)
+		tw.SetAlignment(tablewriter.ALIGN_LEFT)
+		tw.Render()
+	}
+}
+
+func tailDDB(tables []string, outputer chan <- outputItem) {
+	streamSvc, dynamoSvc := initDDB()
+
+	checkTables(tables, dynamoSvc)
+
+	enableStreams(tables, dynamoSvc)
+
+	wg := sync.WaitGroup{}
+	for _, tt := range tables {
+		wg.Add(1)
+		go tailDDBTable(tt, streamSvc, dynamoSvc, outputer)
+	}
+}
+
+func enableStreams(tables []string, dynamoSvc *dynamodb.DynamoDB) {
+	for _, tt := range tables {
+		dto, err := dynamoSvc.DescribeTable(&dynamodb.DescribeTableInput{TableName: aws.String(tt)})
+		if err != nil{
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if *dto.Table.StreamSpecification.StreamEnabled && *dto.Table.StreamSpecification.StreamViewType == dynamodb.StreamViewTypeNewAndOldImages {
+			continue
+		}
+
+		_, err = dynamoSvc.UpdateTable(&dynamodb.UpdateTableInput{
+			StreamSpecification: &dynamodb.StreamSpecification{
+				StreamEnabled: aws.Bool(true),
+				StreamViewType: aws.String(dynamodb.StreamViewTypeNewAndOldImages),
+			},
+			TableName: aws.String(tt),
+		})
+		if err != nil{
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
+
+func checkTables(tables []string, dynamoSvc *dynamodb.DynamoDB) {
+	result, err := dynamoSvc.ListTables(&dynamodb.ListTablesInput{})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	notExistTables := make([]string, 0)
+	for _, tt := range tables {
+		hasTable := false
+		for _, t := range result.TableNames {
+			if tt == *t {
+				hasTable = true
+				break
+			}
+		}
+		if !hasTable {
+			notExistTables = append(notExistTables, tt)
+		}
+	}
+	if len(notExistTables) > 0 {
+		fmt.Printf("tables %s doesn't exist", strings.Join(notExistTables, ","))
+		os.Exit(1)
+	}
+}
+
+func tailDDBTable(table string, streamSvc *dynamodbstreams.DynamoDBStreams, dynamoSvc *dynamodb.DynamoDB, outputer chan <- outputItem) {
+	streamSubscriber := stream.NewStreamSubscriber(dynamoSvc, streamSvc, table)
+	ch, errCh := streamSubscriber.GetStreamDataAsync()
+
+	go func(errCh <-chan error) {
+		for err := range errCh {
+			fmt.Println("Stream Subscriber error: ", err)
+			os.Exit(1)
+		}
+	}(errCh)
+
+	for record := range ch {
+		outputer <- outputItem{
+			tableName: table,
+			action: *record.EventName,
+			changes: translateChanges(record.Dynamodb),
+		}
+	}
+}
+
+func translateChanges(changes *dynamodbstreams.StreamRecord) [][3]string {
+	allKeysMap := make(map[string]struct{})
+	for k, _ := range changes.OldImage {
+		allKeysMap[k] = struct{}{}
+	}
+	for k, _ := range changes.NewImage {
+		allKeysMap[k] = struct{}{}
+	}
+	allKeys := make([]string, 0)
+	for k, _ := range allKeysMap {
+		allKeys = append(allKeys, k)
+	}
+
+	ret := make([][3]string, len(allKeys))
+
+	for i, k := range allKeys {
+		oldVal := translateValue(changes.OldImage[k])
+		newVal := translateValue(changes.NewImage[k])
+
+		ret[i] = [3]string{k, oldVal, newVal }
+	}
+
+	return ret
+}
+
+func translateValue(value *dynamodb.AttributeValue) string {
+	if value == nil {
+		return "undefined"
+	}
+
+	v := strings.ReplaceAll(value.String(), "{\n  ", "{")
+	v = strings.ReplaceAll(v, "\n}", "}")
+	return v
+}
+
+func initDDB() (*dynamodbstreams.DynamoDBStreams, *dynamodb.DynamoDB){
+	sess, err := session.NewSession(&aws.Config{
+		Endpoint:    aws.String("http://localhost:8000"),
+		Region:      aws.String("local"),
+		Credentials: credentials.NewStaticCredentials("a", "b", "c"),
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	streamSvc := dynamodbstreams.New(sess)
+	dynamoSvc := dynamodb.New(sess)
+
+	return streamSvc, dynamoSvc
+}
